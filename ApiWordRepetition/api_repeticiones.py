52bea05e39b92dbce80d155306f7dd5bf4388187
Sebastian Diaz <sebajdiaz@hotmail.com>
2025-09-10 07:20:38 -0300

    agrego posibilidad de ignorar palabras comunes en conteo de repeticiones

--- ApiWordRepetition/api_repeticiones.py
+++ ApiWordRepetition/api_repeticiones.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.params import Query
 from pydantic import BaseModel
 from collections import Counter
+import spacy
+from spacy.tokens import Token
+
+nlp = spacy.load("es_core_news_sm")
 
 app = FastAPI(title="Detección de repetición de palabras", version="1.0")
 
@@ -8,9 +13,41 @@ app = FastAPI(title="Detección de repetición de palabras", version="1.0")
 class TextoEntrada(BaseModel):
     texto: str
 
+def eliminar_signos_de_puntuacion(tokens: list[Token]) -> list[Token]:
+    return [token for token in tokens if not token.is_punct]
 
-def contar_palabras_repetidas(texto: str) -> dict[str, str]:
-    contador_de_palabras = Counter(texto.lower().split())
+def es_palabra_frecuente(token: Token) -> bool:
+    # si es articulo
+    if "Art" in token.morph.get("PronType"):
+        return True
+    # si es preposicion
+    if token.pos_ == "ADP":
+        return True
+    if token.pos_ == "PRON":
+        return True
+    # si es conjuncion coordinante
+    if token.pos_ == "CCONJ":
+        return True
+    # si es conjuncion subordinante
+    if token.pos_ == "SCONJ":
+        return True
+    return False
+
+def eliminar_palabras_frecuentes(tokens: list[Token]) -> list[Token]:
+    return [token for token in tokens if not es_palabra_frecuente(token)]
+
+
+def filtrar_tokens(tokens: list[Token], sin_palabras_frecuentes: bool) -> list[Token]:
+    tokens = eliminar_signos_de_puntuacion(tokens)
+
+    if sin_palabras_frecuentes:
+        tokens = eliminar_palabras_frecuentes(tokens)
+
+    return tokens
+
+
+def contar_palabras_repetidas(palabras: list[str]) -> dict[str, int]:
+    contador_de_palabras = Counter(palabras)
 
     resultado = {
         palabra: cantidad_de_apariciones
@@ -21,7 +58,8 @@ def contar_palabras_repetidas(texto: str) -> dict[str, str]:
     resultado_ordenado_descendente_por_cantidad_de_repeticiones = {
         palabra: cantidad_apariciones
         for palabra, cantidad_apariciones in sorted(
-            resultado.items(), key=lambda item: item[1], reverse=True)
+            resultado.items(), key=lambda item: item[1], reverse=True
+        )
     }
 
     return resultado_ordenado_descendente_por_cantidad_de_repeticiones
@@ -29,8 +67,20 @@ def contar_palabras_repetidas(texto: str) -> dict[str, str]:
 
 # Endpoint principal: POST /repeticiones
 @app.post("/repeticiones")
-def detectar(entrada: TextoEntrada):
-    return contar_palabras_repetidas(entrada.texto)
+def detectar(
+    entrada: TextoEntrada,
+    sin_palabras_frecuentes: bool = Query(
+        False, description="Ignorar artículos, pronombres, preposiciones y conjunciones"
+    )
+):
+    doc = nlp(entrada.texto)
+
+    tokens = [token for token in doc]
+    tokens = filtrar_tokens(tokens, sin_palabras_frecuentes)
+    palabras = [token.text for token in tokens]
+    # falta ver mayusculas y palabras a singular
+
+    return contar_palabras_repetidas(palabras)
 
 # Endpoint de prueba
 @app.get("/")
