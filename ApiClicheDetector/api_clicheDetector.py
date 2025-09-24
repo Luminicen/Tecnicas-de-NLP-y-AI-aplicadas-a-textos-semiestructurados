4d20eeea4e7597a8c785f8b6eaff97b17c706c35
MauricioFRR <mauri030901@gmail.com>
2025-09-24 17:48:21 -0300

    Test listos y correccion de codigo para hacerlo mas fiable

--- ApiClicheDetector/api_clicheDetector.py
+++ ApiClicheDetector/api_clicheDetector.py
@@ -1,50 +1,48 @@
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
 from rapidfuzz import fuzz
 import spacy
-
-
-
-
 
 cliches= [
     "quiero poder",
     "Para poder usar el sistema",
-    "quiero administrar",
-    "quiero un sistema fácil de usar",
-    "Quiero poder hacer clic en un botón",
+    "para administrar",
+    "un sistema fácil de usar",
+    "poder hacer clic en un botón",
     "Para mejorar la experiencia del usuario",
     "Para aumentar la productividad",
     "Para que sea más rápido y fácil",
     "Para que funcione mejor",
     "Para que sea más intuitivo",
-    "quiero gestionar todo el sistema",
-    "quiero guardar datos en la base de datos",
-    "quiero refactorizar el código",
-    "quiero implementar la API",
-    "quiero probar el sistema",
-    "quiero que sea más bonito",
-    "quiero un diseño moderno",
-    "quiero que sea seguro",
-    "quiero que sea escalable",
-    "quiero poder exportar todo",
-    "quiero acceder a todo desde cualquier lugar",
-    "quiero que nunca falle",
-    "quiero que cargue rápido",
-    "quiero que sea compatible con todo",
-    "quiero que siempre funcione",
-    "quiero que nunca falle"
-    "quiero que no tenga errores",
-    "quiero que la base de datos guarde la información",
-    "quiero conectarme al servidor",
-    "quiero administrar usuarios y permisos",
-    "quiero una aplicación que sea la mejor",
-    "quiero que sea más eficiente",
-    "quiero personalizar todo",
-    "quiero una interfaz atractiva",
+    "gestionar todo el sistema",
+    "guardar datos en la base de datos",
+    "refactorizar el código",
+    "implementar la API",
+    "probar el sistema",
+    "que sea más bonito",
+    "un diseño moderno",
+    "que sea seguro",
+    "que sea escalable",
+    "poder exportar todo",
+    "acceder a todo desde cualquier lugar",
+    "que nunca falle",
+    "que cargue rápido",
+    "que sea compatible con todo",
+    "que siempre funcione",
+    "que nunca falle"
+    "que no tenga errores",
+    "que la base de datos guarde la información",
+    "poder conectarme al servidor",
+    "administrar usuarios y permisos",
+    "una aplicación que sea la mejor",
+    "que sea más eficiente",
+    "personalizar todo",
+    "una interfaz atractiva",
     "quiero simplicidad",
-    "quiero que sea todo más claro",
-    "quiero que se integre con cualquier cosa",
-    "quiero que sea moderno y actual",
-    "quiero que el sistema haga todo automáticamente"
+    "que sea todo más claro",
+    "que se integre con cualquier cosa",
+    "que sea moderno y actual",
+    "que el sistema haga todo automáticamente"
   ]
 
 
@@ -52,25 +50,35 @@ app = FastAPI()
 
 nlp = spacy.load("es_dep_news_trf")
 
+
+class TextoEntrada(BaseModel):
+    texto: str
+
+@app.post("/detectar_cliches/")
+def detectar_cliches_endpoint(entrada: TextoEntrada):
+    resultado = detectar_cliches(entrada.texto, cliches, nlp)
+    return {"cliches_encontrados": resultado}
+
+
 def lematizar(texto, nlp):
     doc = nlp(texto.lower())
     return " ".join([token.lemma_ for token in doc if not token.is_punct]   )
 
-def detectar_fuzzy(texto, lista_cliches, nlp, umbral=70):
+def detectar_cliches(texto, lista_cliches, nlp, umbral=70):
     texto_lemmas = lematizar(texto, nlp)
     
     encontrados = []
     for cliche in lista_cliches:
         cliche_lemmas = lematizar(cliche, nlp)
-        valor = fuzz.partial_ratio(cliche_lemmas, texto_lemmas)
+        valor = fuzz.token_set_ratio(cliche_lemmas, texto_lemmas)
+
+        #valor = fuzz.partial_ratio(cliche_lemmas, texto_lemmas)
         
         if valor >= umbral:
             print(valor)
             encontrados.append(cliche)
 
-    if len(encontrados) == 0:
-        return "No se encontraron cliches"
     return encontrados
 
 texto = "Como tester, quiero ser capaz de ejecutar pruebas automáticas en diferentes entornos y con distintos volúmenes de datos, para que el sistema sea compatible con todo, más robusto y nunca falle en producción"
-print(detectar_fuzzy(texto, cliches, nlp))
+#print(detectar_cliches(texto, cliches, nlp))
