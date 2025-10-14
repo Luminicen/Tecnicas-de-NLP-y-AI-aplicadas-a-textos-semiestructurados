26ddac69498128917e2c0e0a7dd10095eae2ca8a
Enzo Giandon <enzogiandon@alu.ing.unlp.edu.ar>
2025-10-14 17:46:53 -0300

    Cambio de nombre

--- ApiTenses/deteccion_verbos_api.py
+++ ApiTenses/deteccion_verbos_api.py
@@ -2,10 +2,10 @@ from builtins import str
 from fastapi import FastAPI 
 import sys
 
-import detección_de_verbos
+import ApiTenses.deteccion_verbos as deteccion_verbos
 
 app = FastAPI()
 
 @app.get("/detección_de_verbos/")
 def verificacion(texto: str):
-    return detección_de_verbos.detectar_tiempo_verbal(texto)
+    return deteccion_verbos.detectar_tiempo_verbal(texto)
